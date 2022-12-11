9959bb1d0fac4694f995de2b3b4333202f24464f
gabrielle-1 <gabriellerodriguesmd@gmail.com>
2022-12-11 17:11:03 -0300

    Creating and saving emails

---
+++ src/main/java/com/ms/email/services/EmailService.java
@@ -0,0 +1,17 @@
+package com.ms.email.services;
+
+import com.ms.email.models.EmailModel;
+import com.ms.email.repositories.EmailRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    EmailRepository emailRepository;
+
+    public EmailModel sendEmail(EmailModel emailModel) {
+        return this.emailRepository.save(emailModel);
+    }
+}
